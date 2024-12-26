098b2b5d3ba8c8af51c10c3e00dc2d667bb0ccef
ROSTAND LELE <rostoinfocus@gmail.com>
2024-12-26 19:16:33 +0100

    pourcentage
    pourcentage

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="container">
    <h1 style="font-size:2rem">{{ quizData.title}}</h1> 
-   <Progress
+   
+    <Progress
     :value="step"
     :max="quizData.questions.length -1"
     class="mt"
     />
+    
+    <span style="color:rgb(150,222,0);">{{ pourcentage }}%</span>
+  
     <Question
       :key="question.question"
       :question="question" v-if="state==='question'" 
@@ -16,7 +20,7 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
+import { computed, ref } from 'vue';
 import Progress from './Progress.vue';
 import Question from './Question.vue';
 import Recap from './Recap.vue';
@@ -39,10 +43,15 @@ const addAnswer =(answer) =>{
     
   }
 }
+
+const pourcentage = computed(() => {
+  return Math.round((step.value / (props.quizData.questions.length - 1)) * 100);
+});
 </script>
 
 <style  scoped>
   .mt{
     margin-top:2rem;
   }
+  
 </style>
